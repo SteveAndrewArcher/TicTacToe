2b9ab15875a97c421d01671ccbdd736ba0cefeab
Steve Archer <SteveAndrewArcher@gmail.com>
2017-07-17 16:30:45 -0500

    Clean up code

--- Player.java
+++ Player.java
@@ -1,30 +1,29 @@
 public class Player
 {
-   private char letter;
-   private String name;
-   private boolean human;
+	private char letter;
+	private String name;
+	private boolean human;
+
+	public Player(char letter, String name, boolean human)
+	{
+		this.letter = letter;
+		this.name = name;
+		this.human = human;
+	}
    
-   public Player(char letter, String name, boolean human)
-   {
-      this.letter = letter;
-      this.name = name;
-      this.human = human;
-   }
+	public char getPlayerLetter()
+	{
+		return this.letter;
+	}
    
-   public char getPlayerLetter()
-   {
-      return this.letter;
-   }
+	public String getPlayerName()
+	{
+		return this.name;
+	}
    
-   public String getPlayerName()
-   {
-      return this.name;
-   }
-   
-   public boolean getHuman()
-   {
-      return this.human;
-   }
-   
+	public boolean getHuman()
+	{
+		return this.human;
+	}   
 }
    
